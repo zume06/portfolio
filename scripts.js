a6dbd3060a173582dee13dd4dfc40f797f600883
zume06 <hashizume.yuuka@g.sp.m.is.nagoya-u.ac.jp>
2026-06-18 14:55:49 +0900

    edit age

--- scripts.js
+++ scripts.js
@@ -10,3 +10,26 @@ function closeImageModal() {
     var modal = document.getElementById('image-modal');
     modal.style.display = 'none';
 }
+
+function calculateAge(birthDate) {
+    const today = new Date();
+    const birth = new Date(birthDate);
+
+    let age = today.getFullYear() - birth.getFullYear();
+
+    const thisYearBirthday = new Date(
+        today.getFullYear(),
+        birth.getMonth(),
+        birth.getDate()
+    );
+
+    if (today < thisYearBirthday) {
+        age--;
+    }
+
+    return age;
+}
+
+const birthday = "2000-06-18";
+
+document.getElementById("age").textContent = calculateAge(birthday);
